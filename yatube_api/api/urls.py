6695051e54d7166c704d372fae0aa95630565bf2
Тимур <51882631+LapkiT@users.noreply.github.com>
2025-04-19 10:18:26 +0300

    repair api pagination

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 from .views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
@@ -18,11 +19,16 @@ router.register('groups', GroupViewSet, basename='groups')
 router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
-    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', include(router.urls)),
+    path('jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path(
-        'v1/token/refresh/',
+        'jwt/refresh/',
         TokenRefreshView.as_view(),
         name='token_refresh'
+    ),
+    path(
+        'jwt/verify/',
+        TokenVerifyView.as_view(),
+        name='token_verify'
     ),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, filters, mixins, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 
 from posts.models import Post, Follow, Group
 from .permissions import IsAuthorOrReadOnly
@@ -16,7 +17,7 @@ from .serializers import (
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
     pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
@@ -26,6 +27,14 @@ class PostViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(group_id=group_id)
         return queryset
 
+    def list(self, request, *args, **kwargs):
+        if 'limit' in request.query_params or 'offset' in request.query_params:
+            return super().list(request, *args, **kwargs)
+        
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
@@ -34,11 +43,13 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = None
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    pagination_class = None
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
@@ -57,6 +68,7 @@ class FollowViewSet(
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = (filters.SearchFilter,)
     search_fields = ('following__username',)
+    pagination_class = None
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
